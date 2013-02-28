a1276574062cad5f72f292b6aecda19bf2482987
nickchy <nickchy1984@gmail.com>
2013-02-28 16:54:20 -0500

    a

--- CareerCup/Chapter1/Chp1Sol.hpp
+++ CareerCup/Chapter1/Chp1Sol.hpp
@@ -1,5 +1,49 @@
+// =====================================================================================
+// 
+//       Filename:  Chp1Sol.hpp
+// 
+//    Description:  Career Cup Exercises Chapter 1 Solution
+// 
+//        Version:  1.0
+//        Created:  02/28/2013 02:10:38 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Nick Yang Cai ((nickchy)), nickchy1984@gmail.com
+//        Company:  
+// 
+// =====================================================================================
 
-#ifndef  CHP1SOL_HPP_INC
-#define  CHP1SOL_HPP_INC
+#ifndef  CHP1SOL_HPP
+#define  CHP1SOL_HPP
 
-#endif   // ----- #ifndef CHP1SOL_HPP_INC  -----
+
+// ===  FUNCTION  ======================================================================
+//         Name:  Fibonacci
+//  Description:  calcualte the nth fibonacci number for question 1.1
+// =====================================================================================
+int Fibonacci (int n)
+{
+    int a = 0;
+    int b = 1;
+    if(n <0)
+    {
+        return -1;
+    }
+    else if( n < 2) 
+    {
+        return n;
+    }
+    else
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            b= a+ b;
+            a = b;
+        }
+    }
+    return b;
+}		// -----  end of function Fibonacci  -----
+
+
+#endif   // ----- #ifndef CHP1SOL_HPP  -----
